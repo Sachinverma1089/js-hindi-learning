77327871a49b8a702cd2b2f807e82a6270026383
Sachinverma1089 <87863276+Sachinverma1089@users.noreply.github.com>
2024-02-23 05:14:21 +0000

    comparisons

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "Sachin"
+/*let score = "Sachin"
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -25,3 +25,40 @@ console.log(booleanIsLoogedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+*/
+
+// ***** Operations *******
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+// console.log(2+2)
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1="hello"
+// let str2 = " sachin"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(typeof("1"+2));
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+
+let gameCounter=100
+console.log(gameCounter++);
