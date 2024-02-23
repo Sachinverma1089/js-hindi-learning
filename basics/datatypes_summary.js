f3ed2630e60ab31fe2e8100f69cc8d52f594d59d
Sachinverma1089 <87863276+Sachinverma1089@users.noreply.github.com>
2024-02-23 05:33:02 +0000

    datatype summary

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types: String, Number, Boolaen, null,undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId)
+
+const bigNumber=65445645564654655665n
+
+
+
+
+// Reference Type (Non primitive)
+
+// Array,Objects,Functions
+
+
+const heroes = ["shaktiman","naagraj","doga"];
+let myObj={
+    name:"Sachin",
+    age:20,
+
+}
+
+const myFunction = function(){
+    console.log("Namaste Duniya!");
+}
+
+console.log(typeof myFunction);
