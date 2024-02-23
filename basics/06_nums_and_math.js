e5eeb7454ed1b74a54dd0617680bc7bfb6582b97
Sachinverma1089 <87863276+Sachinverma1089@users.noreply.github.com>
2024-02-23 08:30:27 +0000

    Math and num

---
+++ basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const score = 400
+
+// const balance = new Number(100.1212121)
+
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.893
+
+// console.log(otherNumber.toPrecision(3));
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++ Maths +++++++++++++
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
+
