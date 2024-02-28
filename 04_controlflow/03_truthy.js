9854ff4e5642eddd3d8f46a38e351cd73d118639
Sachinverma1089 <87863276+Sachinverma1089@users.noreply.github.com>
2024-02-28 08:42:14 +0000

    loops

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -23,3 +23,23 @@ if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 
 }
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
